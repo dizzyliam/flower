b1320c95232961518b76c49574b47174b6c8d083
Liam Scaife <lscaife@protonmail.com>
2021-12-20 23:18:07 +1300

    Updated benchmark

--- benchmarks/bm1.nim
+++ benchmarks/bm1.nim
@@ -6,7 +6,7 @@ var bloom = newBloom(10000000, 0.001)
 
 # Add a bunch of items to it.
 var startTime = cpuTime()
-for i in 0..<10000000:
+for i in 0..<1e7.int:
     bloom.add(i)
 
 # See how long that took.
@@ -14,20 +14,20 @@ echo "10M Insertions: ", cpuTime() - startTime
 
 # Now query a bunch of items.
 startTime = cpuTime()
-for i in 0..<10000000:
+for i in 0..<1e7.int:
     discard i in bloom
 
 # See how long that took.
 echo "10M Queries: ", cpuTime() - startTime
 
 # Empty that bloom filter and create a new one.
-bloom = newBloom(10000000, 0.001)
-var bloom1 = newBloom(10000000, 0.001)
+bloom = newBloom(1e7.int, 0.001)
+var bloom1 = newBloom(1e7.int, 0.001)
 
 # Add a littler bunch of items to both.
-for i in 0..<(10000000 div 2):
+for i in 0..<0.5e7.int:
     bloom.add(i)
-    bloom1.add(i+(10000000 div 2))
+    bloom1.add(i+0.5e7.int)
 
 # Nerge them together
 startTime = cpuTime()
